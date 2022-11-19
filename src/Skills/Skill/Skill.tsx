0bfe6b665eb8f8663868acc059e60b8b329807ac
sanyalu <sanyalu97@gmail.com>
2022-11-19 20:43:32 +0100

    portfolio 2nd lesson, SCSS, media

--- src/Skills/Skill/Skill.tsx
+++ src/Skills/Skill/Skill.tsx
@@ -2,18 +2,21 @@ import React from 'react';
 import styles from './Skill.module.scss'
 
 type PropsType = {
-  title: string
-  description: string
+    title: string
+    description: string
+    children?: React.ReactNode
 }
 
-const Skill:React.FC<PropsType> = (props) => {
-  return (
-    <div className={styles.skill}>
-      <div className={styles.iconContainer}></div>
-      <h3 className={styles.title}>{props.title}</h3>
-      <p className={styles.description}>{props.description}</p>
-    </div>
-  );
+const Skill: React.FC<PropsType> = (props) => {
+    return (
+        <div className={styles.skill}>
+            <div className={styles.iconContainer}>
+                <div className={styles.icon}>{props.children}</div>
+            </div>
+            <h3 className={styles.title}>{props.title}</h3>
+            <p className={styles.description}>{props.description}</p>
+        </div>
+    );
 };
 
 export default Skill;
--- src/Skills/Skills.tsx
+++ src/Skills/Skills.tsx
@@ -3,22 +3,37 @@ import styles from './Skills.module.scss'
 import stylesContainer from '../common/styles/Container.module.css'
 import Skill from "./Skill/Skill";
 import Title from "../common/components/Title/Title";
+import HtmlIcon from "../common/components/svgComponents/HtmlIcon";
+import CssIcon from "../common/components/svgComponents/CssIcon";
+import ReactIcon from "../common/components/svgComponents/ReactIcon";
 
 
 const Skills = () => {
-  return (
-    <div id={'skills'} className={styles.skillsBlock}>
-      <div className={`${stylesContainer.container} ${styles.skillsContainer}`}>
-        <Title text={'Skills'}/>
-        <div className={styles.skills}>
-          <Skill title={'HTML'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, ut!'}/>
-          <Skill title={'CSS'} description={'Lorem ipsum dolor sit amet'}/>
-          <Skill title={'REACT'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, ut!'}/>
-        </div>
-      </div>
+    return (
+        <div id={'skills'} className={styles.skillsBlock}>
+            <div className={`${stylesContainer.container} ${styles.skillsContainer}`}>
+                <Title text={'Skills'}/>
+                <div className={styles.skills}>
+                    <Skill
+                        title={'HTML'}
+                        description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, ut!'}>
+                        <HtmlIcon/>
+                    </Skill>
+                    <Skill
+                        title={'CSS'}
+                        description={'Lorem ipsum dolor sit amet'}>
+                        <CssIcon/>
+                    </Skill>
+                    <Skill
+                        title={'REACT'}
+                        description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, ut!'}>
+                        <ReactIcon/>
+                    </Skill>
+                </div>
+            </div>
 
-    </div>
-  );
+        </div>
+    );
 };
 
 export default Skills;
